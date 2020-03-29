a47091dd069d77ef2eff983224a697674759b50e
Tejas S <41792273+Tejasharish@users.noreply.github.com>
2020-03-29 10:41:47 +0530

    initial commit

---
+++ create_file.py
@@ -0,0 +1,75 @@
+import os
+from urllib.parse import urlparse
+
+
+# Each website is a separate project (folder)
+def create_project_dir(directory):
+    if not os.path.exists(directory):
+        print('Creating directory ' + directory)
+        os.makedirs(directory)
+
+
+# Create queue and crawled files (if not created)
+def create_data_files(project_name, base_url):
+    queue = os.path.join(project_name , 'queue.txt')
+    crawled = os.path.join(project_name,"crawled.txt")
+    if not os.path.isfile(queue):
+        write_file(queue, base_url)
+    if not os.path.isfile(crawled):
+        write_file(crawled, '')
+
+
+# Create a new file
+def write_file(path, data):
+    with open(path, 'w') as f:
+        f.write(data)
+
+
+# Add data onto an existing file
+def append_to_file(path, data):
+    with open(path, 'a') as file:
+        file.write(data + '\n')
+
+
+# Delete the contents of a file
+def delete_file_contents(path):
+    open(path, 'w').close()
+
+
+# Read a file and convert each line to set items
+def file_to_set(file_name):
+    results = set()
+    with open(file_name, 'rt') as f:
+        for line in f:
+            results.add(line.replace('\n', ''))
+    return results
+
+
+# Iterate through a set, each item will be a line in a file
+def set_to_file(links, file_name):
+    with open(file_name,"w") as f:
+        for l in sorted(links):
+            f.write(l+"\n")
+
+
+
+
+
+# Get domain name (example.com)
+def get_domain_name(url):
+    try:
+        results = get_sub_domain_name(url).split('.')
+        return results[-2] + '.' + results[-1]
+    except:
+        return ''
+
+
+# Get sub domain name (name.example.com)
+def get_sub_domain_name(url):
+    try:
+        return urlparse(url).netloc
+    except:
+        return ''
+
+
+
